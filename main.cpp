e293d6576dfe339473789742b71fa119be386b52
eduardosalas1 <44951783+eduardosalas1@users.noreply.github.com>
2020-03-29 19:05:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-
 #include <iostream>
+#include <vector>
+#include "sorts.cpp"
 using namespace std;
+template<class T>
+void print_array(vector<T> * v){
+    for(auto i : *v){
+        cout<<i<<',';
+    }
+}
 
-int main(int, char*[]){
-    return 1;
-}//repo
+int main() {
+    auto*prueba = new vector<int>();
+    *prueba = {3,8,5,1,6};
+    sorting<int> p(prueba);
+    p.Merge_sort(0,4);//index inicial y final
+    print_array(prueba);
+    return 0;
+}
