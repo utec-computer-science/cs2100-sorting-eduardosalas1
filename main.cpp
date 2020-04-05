4796bb16d1f160e667ca95e3e1c2fe8be8862dd6
Eduardo <eduardo.salas@utec.edu.pe>
2020-04-05 16:44:52 -0500

    Comentarios

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ void print_array(vector<T> * v){
         cout<<i<<',';
     }
 }
-
+//dentro de Quick_sort() y Merge_sort() van como parametros, el primer indice y el ultimo(a ordenar) .
 int main() {
     auto*prueba = new vector<int>();
     *prueba = {3,8,5,1,6};
     sorting<int> p(prueba);
     p.Merge_sort(0,4);//index inicial y final
     print_array(prueba);
-    return 0;
+    return 0;//hola que tal
 }
